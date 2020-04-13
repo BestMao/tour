441efdab1c516411538aeb4cb26ce5325cf1aa2f
BestMao <936137804@qq.com>
2020-04-13 10:50:26 +0800

    first

---
+++ src/components/Home/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>home</div>
+    <home-header></home-header>
+  </div>
+
+</template>
+<script>
+import HomeHeader from './components/HomeHeader.vue'
+export default {
+  components: {
+    HomeHeader
+  }
+}
+</script>
+<style lang="less">
+</style>
---
+++ src/components/Home/components/HomeHeader.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>homeheader</div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="less">
+</style>
